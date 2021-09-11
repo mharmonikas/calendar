e007867da7686b48acb80deb96e62afe984e7396
Martin <martin_n25@yahoo.com>
2021-09-11 20:13:34 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h2>Calendar</h2>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {}
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,574 @@
 <template>
   <div>
-    test
+    <p class="mb-10 mx-auto mt-4 text--accent-4 text-center" width="344">List of calendars</p>
+    <v-card
+        v-for="(calendar, index) in calendars"
+        :key="index"
+        class="mx-auto mb-1"
+        max-width="344"
+        outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="text-overline mb-4">
+            {{calendar.clientName}}
+          </div>
+          <v-list-item-title class="text-h5 mb-1">
+
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ calendar.date }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn
+            @click="openDialog(calendar)"
+            outlined
+            rounded
+            text
+        >
+          Send reminder
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-dialog v-model="dialog" width="500">
+      <div>
+        <v-card>
+          <template slot="progress">
+            <v-progress-linear
+                color="deep-purple"
+                height="10"
+                indeterminate
+            ></v-progress-linear>
+          </template>
+
+          <v-img
+              height="250"
+              src="https://www.cnppid.com/wp-content/uploads/2017/10/wateryearcalendar.jpg"
+          ></v-img>
+
+          <v-card-title>Set Reminder</v-card-title>
+
+          <v-card-text>
+            <div class="my-4 text-subtitle-1">
+              {{calendar.clientName}}
+            </div>
+
+            <div>Choose when to send reminder email to this client.</div>
+          </v-card-text>
+
+          <v-divider class="mx-4"></v-divider>
+
+          <v-card-title>Available options</v-card-title>
+
+          <v-card-text>
+            <v-chip-group
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
+            >
+              <v-chip>daily</v-chip>
+
+              <v-chip>weekly</v-chip>
+
+              <v-chip>monthly</v-chip>
+
+              <v-chip>yearly</v-chip>
+            </v-chip-group>
+          </v-card-text>
+
+          <v-card-title>Send reminder emails until:</v-card-title>
+          <v-card-text>
+            <v-menu
+                ref="showDate"
+                v-model="showDate"
+                :close-on-content-click="false"
+                :return-value.sync="sendUntil"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                    v-model="sendUntil"
+                    label="Picker in menu"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                  v-model="sendUntil"
+                  no-title
+                  scrollable
+              >
+                <v-spacer></v-spacer>
+                <v-btn
+                    text
+                    color="primary"
+                    @click="showDate = false"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                    text
+                    color="primary"
+                    @click="$refs.showDate.save(sendUntil)"
+                >
+                  OK
+                </v-btn>
+              </v-date-picker>
+            </v-menu>
+          </v-card-text>
+<!--          <v-date-picker v-model="sendUntil"></v-date-picker>-->
+
+          <v-card-actions>
+            <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="sendReminder"
+            >
+              Send reminder
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </v-dialog>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-}
-</script>
+  const axios = require('axios').default;
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+  export default {
+    name: 'Home',
+
+    components: {
+    },
+
+    data() {
+      return {
+        dialog: false,
+        calendars: [
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+          {
+            clientName: 'test',
+            date: new Date(),
+          },
+        ],
+        calendar: {},
+        selection: [],
+        sendUntil: null,
+        showDate: false,
+      }
+    },
+
+    methods: {
+      openDialog(calendar) {
+        this.calendar = calendar
+        this.dialog = true
+      },
+
+      sendReminder() {
+        axios.post('backend-route', {calendar: this.calendar, selection: this.selection})
+      }
+    }
+  }
+</script>
